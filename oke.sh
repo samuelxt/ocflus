e658766105ab57af3a7b30d3a130baf139696a50
samuelxt <72672319+samuelxt@users.noreply.github.com>
2020-10-11 04:37:56 +0700

    Create oke.sh

---
+++ oke.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+	sudo apt-get update && apt-get upgrade -y && apt-get install htop -y
+	sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
+	sudo apt-get install software-properties-common
+	sudo add-apt-repository ppa:ondrej/php
+	sudo apt-get install php
+	sudo apt-get install unzip
+	
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/flussonic-python_18.11_amd64.deb
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/flussonic-ffmpeg-base_4.6_amd64.deb
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/flussonic-ffmpeg_4.6_amd64.deb
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/flussonic-erlang_21.1.4_amd64.deb
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/flussonic_18.12.1_amd64.deb
+	wget https://raw.githubusercontent.com/samuelxt/ocflus/blob/main/hack.zip
+	
+	unzip hack.zip
+	dpkg -i flussonic-python_18.11_amd64.deb
+	dpkg -i flussonic-ffmpeg-base_4.6_amd64.deb
+	dpkg -i flussonic-ffmpeg_4.6_amd64.deb
+	dpkg -i flussonic-erlang_21.1.4_amd64.deb
+	dpkg -i flussonic_18.12.1_amd64.deb
+	
+	/etc/init.d/apache2 restart
+	/etc/init.d/flussonic start
